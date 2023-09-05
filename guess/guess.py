cceb8d1500a5dc77b8f7c609c494c22ced34de97
Melvin Lindholm <meli0703@storsjogymnasiet.se>
2023-09-05 10:43:28 +0200

    ADD guessing functionality + right and wrong

--- guess/guess.py
+++ guess/guess.py
@@ -1,7 +1,25 @@
 import random
-print("\n\n")
+print("\n_________________________________________________\n")
 print(".:Gissa Talet:.")
 print("gissa ett tal mellan 1 och 10, 3 försök!\n")
 
 slumptal = random.randint(1, 10)
 print(slumptal)
+i=0
+hittat = False
+
+while i<3:
+    gissatal = input("Gissa ett tal ")
+    if slumptal == int(gissatal):
+        hittat = True
+        print("Rätt svar!")
+        break
+
+    i += 1
+if hittat:
+        print("Grattis!")
+
+else:
+        print("Fel svar loser!")
+
+print("_______________________________________________________")
