2b9781158d0df642be90606f800fcce2d8a0d26b
Melvin Lindholm <meli0703@storsjogymnasiet.se>
2023-10-24 11:25:08 +0200

    made a functional homework thing

---
+++ homework/homework.py
@@ -0,0 +1,44 @@
+import os
+def main ():
+    os.system("cls")
+    print(":homework:")
+    homeworks = {}
+    amount_correct = 0
+
+    while True:
+        swe_homework = input("\n\tType in your Swedish Homework: ")
+        eng_homework = input("\tType the respective Swedish word in English: ")
+
+
+        homeworks[swe_homework] = eng_homework
+
+        add = input("\n\tAdd one more? y/n: ")
+
+        if add == "n":
+            os.system("cls")
+            break
+        if add == "y":
+            os.system("cls")
+            print("\tWhat else?")
+
+
+    while True:
+        print(f"\tAmount of homeworks = {len(homeworks)}")
+
+        for homework in homeworks:
+            answer = input(f"\n\t{homework}:")
+
+            if answer == homeworks[homework]:
+                print("\tCorrect!")
+            else:
+                print(f"\tThe correct answer is :{homeworks[homework]}")
+
+        one_more = input("\n\tDo the reherse again? y/n: ")
+
+
+        if one_more == "n":
+            os.system("cls")
+            break
+
+
+main()
